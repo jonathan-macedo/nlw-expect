03f3e122bfb5f7df3451ba26048b5fbc60c8c012
Jonathan Macedo Castro <jonathan_maccas@outlook.com>
2024-02-06 15:14:17 -0300

    feat: inicializado o projeto e finalizando a aula 01 do evento da Rocketseat

---
+++ src/app.tsx
@@ -0,0 +1,35 @@
+import logo from './assets/logo-nlw-expect.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+export function App() {
+  return (
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt="NLW Expect oferecido pela Rocketseat" />
+      <form className='w-full'>
+        <input
+          type="text"
+          placeholder='Busque suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tighter placeholder:text-slate-500 outline-none'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700' />
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+
+  )
+}
